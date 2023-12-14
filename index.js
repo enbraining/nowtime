5d24a91eb6c8e0934c5a09c6515bf4ad48a7f639
enbraining <me@xolving.com>
2023-12-14 14:39:31 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// MTE4NDcyOTMwMjA4MDcwMDQzNg.GByRhf.SrkPtHutKuAUvsg2x6nrJ2aBjA2r0Fv43dqacc
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const cron = require('cron');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const channelId = 'YOUR_CHANNEL_ID';  // 메시지를 전송할 채널의 ID
+const cronTime = '38 14 * * *';  // cron 표현식, 여기서는 매일 오전 7시
+
+const job = new cron.CronJob(cronTime, () => {
+  const channel = client.channels.cache.get('1184729949345693708');
+
+  if (channel) {
+    channel.send('특정 시간이 되었습니다! 여기에 보낼 메시지');
+  } else {
+    console.error('채널을 찾을 수 없거나 텍스트 채널이 아닙니다.');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`봇이 성공적으로 실행되었습니다!`);
+  job.start();  // cron 작업 시작
+});
+
+client.login('MTE4NDcyOTMwMjA4MDcwMDQzNg.GByRhf.SrkPtHutKuAUvsg2x6nrJ2aBjA2r0Fv43dqacc');
